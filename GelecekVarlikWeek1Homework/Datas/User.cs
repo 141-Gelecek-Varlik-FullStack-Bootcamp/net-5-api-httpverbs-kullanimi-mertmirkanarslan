aa5ee150d4914ac789b928499ec374a1109e5aa7
Mert Mirkan Arslan <mertmirkanarslan@gmail.com>
2021-11-25 19:10:29 +0300

    Model and Api layers are created
    Model and api layers are created.

---
+++ GelecekVarlikWeek1Homework/Datas/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datas
+{
+    public class User
+    {
+        // id is going to be our key, so we should use the [key] data annotation
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Adınızı girin.")]
+        [MinLength(2), MaxLength(50)]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Soyadınızı girin.")]
+        [MinLength(2), MaxLength(50)]
+        public string Surname { get; set; }
+        
+        [Range(typeof(int),"16","80",ErrorMessage ="Kabul edilen yaş aralığı 16 ile 80 arasıdır.")]
+        public int Age { get; set; }
+        
+        [Required(ErrorMessage = "Lütfen e-mail adresinizi girin.")]
+        [MinLength(10, ErrorMessage ="E-mail adresiniz 10 karakterden küçük olamaz.")]
+        [MaxLength(300, ErrorMessage ="E-mail adresiniz 300 karakterden büyük olamaz.")]
+        [EmailAddress(ErrorMessage ="Lütfen geçerli bir e-mail adresi girin.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Lütfen parola belirleyin.")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ GelecekVarlikWeek1Homework/GelecekVarlikWeek1Homework/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using Datas;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GelecekVarlikWeek1Homework.Controllers
+{
+    //route üzerinde template'e s ekliyoruz, bunun sebebi domain.com/api/user gibi bir kullanım yerine domain.com/api/users şeklinde oluşturmasını istememiz.
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //statik user listesi oluşturuyorum:
+        private static List<User> users = new List<User>()
+        {
+            new User
+            {
+                Id=1,
+                Name="Mert Mirkan",
+                Surname="Arslan",
+                Age=27,
+                Email="mertmirkanarslan@gmail.com",
+                Password="1234567"
+            },
+            new User
+            {
+                Id=2,
+                Name="Vítor Manuel de Oliveira Lopes",
+                Surname="Pereira",
+                Age=53,
+                Email="elitecoach@gmail.com",
+                Password="fenerbahce"
+            },
+            new User
+            {
+                Id=3,
+                Name="Marcelo Alberto",
+                Surname="Bielsa",
+                Age=66,
+                Email="el-loco@gmail.com",
+                Password="sahayimilyonaboldum"
+            },
+            new User
+            {
+                Id=4,
+                Name="Josep 'Pep'",
+                Surname="Guardiola",
+                Age=50,
+                Email="isimgucum@futbol.com",
+                Password="beylerpaslibeyler"
+            },
+            new User
+            {
+                Id=5,
+                Name="Christoph Paul",
+                Surname="Daum",
+                Age=66,
+                Email="gorevehazirim@gazi.com",
+                Password="fenerbahce1907"
+            },
+        };
+
+        //get, post, put, delete işlemleri yapılacak. 
+        // User Login, New User, User Detail
+
+
+        // Users List
+
+
+    }
+}
